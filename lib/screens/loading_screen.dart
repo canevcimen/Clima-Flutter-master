ec393e3d4ee0b328c64022e30f79c7004c9bebf3
can <canevcimen09@gmail.com>
2024-04-30 01:13:57 +0300

    Initial commit

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:clima/services/location.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+
+  @override
+  void initState()
+  {
+    getLocation();
+  }
+
+  void getLocation() async
+  {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // body: Center(
+      //   child: ElevatedButton(
+      //     onPressed: () {
+      //       getLocation();
+      //     },
+      //     child: Text('Get Location'),
+      //   ),
+      // ),
+    );
+  }
+}
---
+++ lib/services/location.dart
@@ -0,0 +1,22 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location{
+
+  double? latitude;
+  double? longitude;
+
+  Future<void> getCurrentLocation() async {
+    try {
+      // LocationPermission permission;
+      // permission = await Geolocator.requestPermission();  //uygulamanın konumunu kullanması için izin alman gereken kod bloğu
+
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+      latitude = position.latitude;
+      longitude = position.longitude;
+    }
+    catch (e) {
+      print(e);
+    }
+  }
+}
